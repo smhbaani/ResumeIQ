7cdc776c68602c25641eb4eb9618241f3c5d76f0
smhbaani <sohal.baani2006@gmail.com>
2026-06-04 11:49:59 +0530

    Update information_extractor.py

--- parser/information_extractor.py
+++ parser/information_extractor.py
@@ -1,16 +1,4 @@
 import re
-import spacy
-
-try:
-    nlp = spacy.load("en_core_web_sm")
-
-except:
-
-    from spacy.cli import download
-
-    download("en_core_web_sm")
-
-    nlp = spacy.load("en_core_web_sm")
 
 SKILLS_DB = [
     "python",
